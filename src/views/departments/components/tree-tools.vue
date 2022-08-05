b1a481a4e6eb9ccb80f1db0d15483391fa4285da
caipaofan <954698621@qq.com>
2022-08-05 14:42:58 +0800

    获取组织架构数据，并进行树形处理

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-row type="flex">
+    <el-col>{{ treeNode.name }}</el-col>
+    <el-col :span="5">
+      <el-row type="flex">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              操作<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <template v-if="!isRoot">
+                <el-dropdown-item>删除子部门</el-dropdown-item>
+                <el-dropdown-item>修改子部门</el-dropdown-item>
+              </template>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'treetools',
+  data() {
+    return {}
+  },
+  props: {
+    treeNode: {
+      type: Object,
+      required: true,
+    },
+    isRoot: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+.box-card {
+  width: 980px;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.el-row {
+  width: 100%;
+}
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <treetools :treeNode="company" :isRoot="true"></treetools>
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+            <template v-slot='{data}'>
+              <treetools :treeNode="data"> </treetools>
+            </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import treetools from './components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      defaultProps: {
+        label: 'name',
+      },
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' },
+      ],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
+    }
   },
-
+  components: {
+    treetools,
+  },
   created() {},
 
-  methods: {}
+  methods: {},
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="scss">
+
+</style>

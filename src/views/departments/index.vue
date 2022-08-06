26e37732b06e5f2d25fda234059c63340855ace7
caipaofan <954698621@qq.com>
2022-08-06 17:33:05 +0800

    完成部门模块

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <el-card class="box-card">
+      <el-card class="box-card" v-loading="loading">
         <treetools :treeNode="company" :isRoot="true" @add='dialogVisible=true'></treetools>
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <template v-slot="{ data }">
@@ -22,6 +22,7 @@ import addDepts from './components/add-depts.vue'
 import { change } from '@/utils'
 import { getDeptsApi } from '@/api/department'
 import treetools from './components/tree-tools.vue'
+import { Loading } from 'element-ui'
 export default {
   data() {
     return {
@@ -35,7 +36,8 @@ export default {
       ],
       company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
       dialogVisible:false,
-      currentNode:{}
+      currentNode:{},
+      loading:false
     }
   },
   components: {
@@ -48,9 +50,11 @@ export default {
 
   methods: {
     async getDepts() {
+      this.loading=true
       const res = await getDeptsApi()
       console.log(res)
       this.departs = change(res.depts, '')
+      this.loading=false
     },
     showAddDept(val){
       this.dialogVisible=true
